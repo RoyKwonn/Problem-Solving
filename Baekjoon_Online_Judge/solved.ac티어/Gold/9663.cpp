85e63b04c4d29b8af8f12220e2f4d42cb7334294
Seokhwan-Kwon <sychar05@gmail.com>
2021-02-06 16:46:14 +0900

    2021-02-06 골드티어 추가

---
+++ Baekjoon_Online_Judge/solved.ac티어/Gold/9663.cpp
@@ -0,0 +1,42 @@
+//
+//  9663.cpp
+//  Problem_solving
+//
+//  Created by Seokhwan Kwon on 2021/02/06.
+//
+
+#include <iostream>
+#define MAX 15
+using namespace std;
+
+int col[MAX];
+int N, total = 0;
+
+bool check(int level)
+{
+    for(int i = 0; i < level; i++)
+        if(col[i] == col[level] || abs(col[level] - col[i]) == level - i)// 대각선이거나 같은 라인
+            return false;
+        //col[i]가 의미하는 것이 X좌표, i가 의미하는것이 Y좌표이므로 차이가 일정하다면 대각선에 있다고 볼 수 있다.
+    return true;
+}
+
+void nqueen(int x)
+{
+    if(x == N)
+        total++;
+    else
+    {
+        for(int i = 0; i < N; i++)
+        {
+            col[x] = i; // 해당 위치에 퀸을 배치
+            if(check(x)) // 유효하다면 다음행의 퀸 배치, 유효하지않다면 다른 위치로 퀸 배치 변경
+                nqueen(x+1);
+        }
+    }
+}
+int main() {
+    cin >> N;
+    nqueen(0);
+    cout << total;
+}
--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 - Algorithm
     - [Baekjoon Online Judge](https://www.acmicpc.net/) 
         - 단계별로 풀어보기
+        - [solved.ac티어](https://solved.ac/)
+            - Gold
 
 
 ## Used language
