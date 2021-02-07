b3c04f77de9672ba160ede3324be8c7a3d572e36
Seokhwan-Kwon <sychar05@gmail.com>
2021-02-08 02:13:15 +0900

    2021-02-08

--- Baekjoon_Online_Judge/단계별로_풀어보기/17-스택/1874.cpp
+++ Baekjoon_Online_Judge/단계별로_풀어보기/17-스택/1874.cpp
@@ -7,15 +7,52 @@
 
 #include <iostream>
 #include <stack>
+#include <vector>
 using namespace std;
 
 int main(int argc, const char * argv[])
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
     cout.tie(NULL);
+    cin.tie(NULL);
     
+    int n;
+    stack<int> st;
+    vector<int> v;
+    vector<char> d;
+    cin >> n;
+        
+    for(int i = 0; i < n; i++)
+    {
+        int k;
+        cin >> k;
+        v.push_back(k);
+    }
     
+    vector<int>::iterator it;
+    it = v.begin();
     
+    for(int i = 1; i <= n; i++) {
+        st.push(i);
+        d.push_back('+');
+        while(!st.empty())
+        {
+            if(*it != st.top()) break;
+            else {
+                st.pop();
+                d.push_back('-');
+                ++it;
+            }
+        }
+    }
+    if(st.empty()) {
+        for(int i=0; i < d.size(); ++i)
+        {
+            cout << d[i] << "\n";
+        }
+    } else {
+        cout << "NO";
+    }
+
     return 0;
 }
