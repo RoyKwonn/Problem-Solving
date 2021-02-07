03add8b9e826a3b7bded45590a6046f28d97dcb7
Seokhwan-Kwon <sychar05@gmail.com>
2021-02-08 01:48:33 +0900

    2021-02-08

--- Baekjoon_Online_Judge/단계별로_풀어보기/16-스택/10828.cpp
+++ Baekjoon_Online_Judge/단계별로_풀어보기/16-스택/10828.cpp
@@ -6,6 +6,8 @@
 //
 
 #include <iostream>
+#include <stack>
+#include <string>
 using namespace std;
 
 int main(int argc, const char * argv[])
@@ -14,7 +16,43 @@ int main(int argc, const char * argv[])
     cout.tie(NULL);
     cin.tie(NULL);
     
+    int N;
+    cin >> N;
     
+    stack<int> st; // int type의 stack 선언.
+    string str;
+    
+    for(int i=0; i<N; i++)
+    {
+        cin >> str;
+        
+        if(str == "push") {
+            int num;
+            cin >> num;
+            st.push(num);
+        } else if(str == "pop") {
+            if(!st.empty()) {
+                cout << st.top() << "\n";
+                st.pop();
+            } else {
+                cout << "-1" << "\n";
+            }
+        } else if(str == "size") {
+            cout << st.size() << "\n";
+        } else if(str == "empty") {
+            if(st.empty()) {
+                cout << "1" << "\n";
+            } else {
+                cout << "0" << "\n";
+            }
+        } else if (str == "top") {
+            if(!st.empty()) {
+                cout << st.top() << "\n";
+            } else {
+                cout << "-1" << "\n";
+            }
+        }
+    }
     
     return 0;
 }
