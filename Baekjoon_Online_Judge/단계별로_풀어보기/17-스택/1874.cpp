3af1209de6565fafdb728f42d8b54384c73e85fb
Seokhwan-Kwon <sychar05@gmail.com>
2021-02-08 02:29:34 +0900

    2021-02-08

--- Baekjoon_Online_Judge/단계별로_풀어보기/17-스택/1874.cpp
+++ Baekjoon_Online_Judge/단계별로_풀어보기/17-스택/1874.cpp
@@ -12,16 +12,22 @@ using namespace std;
 
 int main(int argc, const char * argv[])
 {
+    // 얌생이 내용
     ios_base::sync_with_stdio(false);
     cout.tie(NULL);
     cin.tie(NULL);
     
+    
+    
     int n;
-    stack<int> st;
-    vector<int> v;
-    vector<char> d;
+    stack<int> st;  // 스택
+    vector<int> v;  // 입력된 수를 차례대로 저장
+    vector<char> ans; // 연산 결과를 내는 기호
     cin >> n;
-        
+    
+    
+    
+    // 우선 숫자를 차례대로 받기
     for(int i = 0; i < n; i++)
     {
         int k;
@@ -29,26 +35,33 @@ int main(int argc, const char * argv[])
         v.push_back(k);
     }
     
-    vector<int>::iterator it;
+    vector<int>::iterator it; // 반복자 설정
     it = v.begin();
     
-    for(int i = 1; i <= n; i++) {
+    // n개에 대한 차례대로 설정하기
+    for(int i = 0; i < n; i++)
+    {
+        // push할땐 '+'
         st.push(i);
-        d.push_back('+');
+        ans.push_back('+');
+        
+        // 스택안에 값이 있다면?
         while(!st.empty())
         {
-            if(*it != st.top()) break;
+            // it에 v의 첫번째값이 저장되어 있다.
+            if(*it != st.top()) break; // 값이 같다면 pop을하지 않는다.
             else {
                 st.pop();
-                d.push_back('-');
+                ans.push_back('-'); // pop할때는 '-'
                 ++it;
             }
         }
     }
     if(st.empty()) {
-        for(int i=0; i < d.size(); ++i)
+        for(int i=0; i < ans.size(); ++i)
         {
-            cout << d[i] << "\n";
+            // 저장된 문자들을 불러온다.
+            cout << ans[i] << "\n";
         }
     } else {
         cout << "NO";
