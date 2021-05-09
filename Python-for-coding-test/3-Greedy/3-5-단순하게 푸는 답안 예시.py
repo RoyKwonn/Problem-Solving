88148bfc021b07de3a2f14e81e3983fdee7c4805
Seokhwan-Kwon <sychar05@gmail.com>
2021-05-09 10:00:41 +0900

    python-fro-codoing-test

---
+++ Python-for-coding-test/3-Greedy/3-5-단순하게 푸는 답안 예시.py
@@ -0,0 +1,21 @@
+n, k = map(int, input().split())
+result = 0
+
+# N이 K로 나누어 떨어지지 않는다면 N에서 1씩 빼기
+while n >= k:
+    # N이 K로 나누어 떨어지지 않는다면 N에서 1씩 빼기
+    while n % k != 0:
+        n -= 1
+        result += 1
+
+    # K로 나누기
+    n //= k
+    result += 1
+
+
+# 마지막으로 남은 수에 대하여 1씩 빼기
+while n > 1:
+    n -= 1
+    result += 1
+
+print(result)
