5a7ece33045753c8429227380d1d830c9d82e2f0
Seokhwan-Kwon <sychar05@gmail.com>
2021-05-23 22:28:02 +0900

    4주차

---
+++ Python-for-coding-test/5-DFS_BFS/Q21-인구 이동.py
@@ -0,0 +1,72 @@
+from sys import stdin
+from collections import deque
+
+# 국경선을 공유하는 두 나라의 인구 차이가 L명 이상, R명 이하라면, 두 나라가 공유하는 국경선을 오늘 하루동안 연다.
+# 위의 조건에 의해 열어야하는 국경선이 모두 열렸다면, 인구 이동을 시작한다.
+# 국경선이 열려있어 인접한 칸만을 이용해 이동할 수 있으면, 그 나라를 오늘 하루 동안은 연합이라고 한다.
+# 연합을 이루고 있는 각 칸의 인구수는 (연합의 인구수) / (연합을 이루고 있는 칸의 개수)가 된다. 편의상 소수점은 버린다.
+# 연합을 해체하고, 모든 국경선을 닫는다.
+
+n, l, r = map(int, stdin.readline().split())
+graph = [list(map(int, stdin.readline().split())) for _ in range(n)]
+
+move = [[-1,0],[1,0],[0,-1],[0,1]]
+
+
+result = 0
+
+# 특정 위치에서 출발하여 모든 연합을 체크한 뒤에 데이터 갱신
+def process(x, y, index):
+
+    # (x, y)의 위치와 연결된 나라(연합) 정보를 담는 리스트
+    united = []
+    united.append((x, y)) # 시작하는 나라
+
+    # 너비 우선 탐색 (BFS)을 위한 큐 라이브러리 사용
+    q = deque()
+    q.append((x, y))
+    union[x][y] = index # 현재 연합의 번호 할당
+    summary = graph[x][y] # 현재 연합의 전체 인구 수
+    count = 1 # 현재 연합의 국가 수
+
+    # 큐가 빌 때까지 반복(BFS)
+    while q:
+        x, y = q.popleft()
+        # 현재 위치에서 4가지 방향을 확인하며
+        for [dx, dy] in move:
+            nx = x + dx
+            ny = y + dy
+            # 바로 옆에 있는 나라를 확인하여
+            if 0 <= nx < n and 0 <= ny < n and union[nx][ny] == -1:
+                # 옆에 있는 나라와 인구 차이가 L명 이상, R명 이하라면
+                if l <= abs(graph[nx][ny] - graph[x][y]) <= r:
+                    q.append((nx, ny))
+                    # 연합에 추가하기
+                    union[nx][ny] = index
+                    summary += graph[nx][ny]
+                    count += 1
+                    united.append((nx, ny))
+    # 연합 국가끼리 인구를 분배
+    for i, j in united:
+        graph[i][j] = summary // count
+
+
+total_count = 0
+
+# 더 이상 인구 이동을 할 수 없을 때까지 반복
+while True:
+    union = [[-1] * n for _ in range(n)]
+    index = 0
+
+    for i in range(n):
+        for j in range(n):
+            if union[i][j] == -1: # 해당 나라가 아직 처리되지 않았다면
+                process(i, j, index)
+                index += 1
+    # 모든 인구 이동이 끝난 경우
+    if index == n * n:
+        break
+    total_count += 1
+
+# 인구 이동 횟수 출력
+print(total_count)
