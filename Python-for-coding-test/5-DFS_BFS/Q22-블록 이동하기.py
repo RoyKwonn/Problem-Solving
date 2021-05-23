367c46f361b907c0e6d319105ec979c5d5a5baff
Seokhwan-Kwon <sychar05@gmail.com>
2021-05-23 23:26:37 +0900

    4주차

--- Python-for-coding-test/5-DFS_BFS/Q22-블록 이동하기.py
+++ Python-for-coding-test/5-DFS_BFS/Q22-블록 이동하기.py
@@ -5,7 +5,7 @@
 
 from collections import deque
 
-def get_next_pos(pos, board):
+def get_next_pos(pos, board): # 이동가능한 모든 가능성 일아보기
     next_pos = [] # 반환 결과 (이동 가능한 위치들)
 
     pos = list(pos) # 현재 위치 정보를 리스트로 변환 (집합 → 리스트)
@@ -14,24 +14,26 @@ def get_next_pos(pos, board):
 
     # (상, 하, 좌, 우)로 이동하는 경우에 대해서 처리
     move = [[-1,0],[1,0],[0,-1],[0,1]]
-
     for dx, dy in move: # 두개의 점에 대해서 이동한다.
         pos1_next_x, pos1_next_y, pos2_next_x, pos2_next_y = pos1_x + dx, pos1_y + dy, pos2_x + dx, pos2_y + dy
         # 이동하고자 하는 두 칸이 모두 비어 있다면
         if board[pos1_next_x][pos1_next_y] == 0 and board[pos2_next_x][pos2_next_y] == 0:
             next_pos.append({(pos1_next_x, pos1_next_y), (pos2_next_x, pos2_next_y)})
+
     # 현재 로봇이 가로로 놓여 있는 경우
     if pos1_x == pos2_x:
         for i in [-1, 1]: # 위쪽으로 회전하거나, 아래쪽으로 회전
             if board[pos1_x + i][pos1_y] == 0 and board[pos2_x + i][pos2_y] == 0: # 위쪽 혹은 아래쪽 두 칸이 모두 비어 있다면
                 next_pos.append({(pos1_x, pos1_y), (pos1_x + i, pos1_y)})
                 next_pos.append({(pos2_x, pos2_y), (pos2_x + i, pos2_y)})
+
     # 현재 로봇이 세로로 놓여 있는 경우
     elif pos1_y == pos2_y:
         for i in [-1, 1]: # 왼쪽으로 회전하거나, 오른쪽으로 회전
             if board[pos1_x][pos1_y + i] == 0 and board[pos2_x][pos2_y + i] == 0: # 왼쪽 혹은 오른쪽 두 칸이 모두 비어 있다면
                 next_pos.append({(pos1_x, pos1_y), (pos1_x, pos1_y + i)})
                 next_pos.append({(pos2_x, pos2_y), (pos2_x, pos2_y + i)})
+
     # 현재 위치에서 이동할 수 있는 위치를 반환
     return next_pos
 
@@ -63,11 +65,12 @@ def solution(board):
     while q:
         pos, cost = q.popleft()
         # (n, n) 위치에 로봇이 도달했다면, 최단 거리이므로 반환
-        if (n, n) in pos:
+        if (n, n) in pos: # 종료조건
             return cost
+
         # 현재 위치에서 이동할 수 있는 위치 확인
-        for next_pos in get_next_pos(pos, new_board):
+        for next_pos in get_next_pos(pos, new_board): # 이동가능한 위치로 차례대로 이동한다.
             # 아직 방문하지 않은 위치라면 큐에 삽입하고 방문 처리
-            if next_pos not in visited:
-                q.append((next_pos, cost + 1))
-                visited.append(next_pos)
+            if next_pos not in visited: # 방문하지 않은곳이라면 BFS에 맞추어 큐에 저장한다.
+                q.append((next_pos, cost + 1)) # 코스트 증가된 상태로 저장하는것이 뽀인트
+                visited.append(next_pos) # 방문처리한다.
