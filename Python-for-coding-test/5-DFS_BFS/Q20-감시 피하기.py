66b08efcc9790c9d5393970dc743f46893527f98
Seokhwan-Kwon <sychar05@gmail.com>
2021-05-23 21:59:52 +0900

    4주차

---
+++ Python-for-coding-test/5-DFS_BFS/Q20-감시 피하기.py
@@ -0,0 +1,85 @@
+# 앞에 연구소 문제와 비슷하다
+
+from sys import stdin
+from itertools import combinations
+
+def watch(x, y, direction):
+    if direction == 0:
+        while y >= 0:
+            if board[x][y] == 'S':
+                return True
+            if board[x][y] == 'O':
+                return False
+            y -= 1
+
+    if direction == 1:
+        while y < n:
+            if board[x][y] == 'S':
+                return True
+            if board[x][y] == 'O':
+                return False
+            y += 1
+
+    if direction == 2:
+        while x >= 0:
+            if board[x][y] == 'S':
+                return True
+            if board[x][y] == 'O':
+                return False
+            x -= 1
+
+    if direction == 3:
+        while x < n:
+            if board[x][y] == 'S':
+                return True
+            if board[x][y] == 'O':
+                return False
+            x += 1
+    return False
+
+# 장애물 설치 이후에, 한 명이라도 학생이 감지되는지 검사
+def process():
+    # 모든 선생의 위치를 하나씩 확인
+    for x, y in teachers:
+        # 4가지 방향으로 학생을 감지할 수 있는지 확인
+        for i in range(4):
+            if watch(x, y, i):
+                return True
+    return False
+
+
+n = int(input())
+
+board = []
+teachers = []
+spaces = []
+
+for i in range(n):
+    board.append(list(stdin.readline().split()))
+    for j in range(n):
+        if board[i][j] == 'T':
+            teachers.append((i, j))
+        if board[i][j] == 'X':
+            spaces.append((i, j))
+
+
+find = False # 학생이 한 명도 감지되지 않도록 설치할 수 있는지의 여부
+
+# 빈 공간에서 3개를 뽑는 모든 조합을 확인
+for data in combinations(spaces, 3):
+    # 장애물들을 설치해보기
+    for x, y in data:
+        board[x][y] = 'O'
+    # 학생이 한 명도 감지되지 않는 경우
+    if not process():
+        # 원하는 경우를 발견한 것임
+        find = True
+        break
+    # 설치된 장애물을 다시 없애기
+    for x, y in data:
+        board[x][y] = 'X'
+
+if find:
+    print('YES')
+else:
+    print('NO')
