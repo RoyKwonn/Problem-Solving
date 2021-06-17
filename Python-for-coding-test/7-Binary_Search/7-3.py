63293cc2a58abd332dc75f23aeaab64d9f66b7c9
Seokhwan-Kwon <sychar05@gmail.com>
2021-06-17 14:37:25 +0900

    2021-06-17

---
+++ Python-for-coding-test/7-Binary_Search/7-3.py
@@ -0,0 +1,23 @@
+# 이진 탐색 소스코드 구현 (반복문)
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        # 찾은 경우 중간점 인덱스 반환
+
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+# 입력
+n, target = list(map(int, input().split()))
+array = list(map(int, input().split()))
+
+result = binary_search(array, target, 0, n - 1)
+if result == None:
+    print("원소가 존재하지 않습니다.")
+else:
+    print(result + 1)
