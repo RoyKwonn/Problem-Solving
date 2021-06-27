3aa9e1c897fa176491501506c2211e2c60f168de
Seokhwan-Kwon <sychar05@gmail.com>
2021-06-27 18:43:18 +0900

    2021-06-27

---
+++ Python-for-coding-test/8-Dynamic_Programming/5-효율적인 화폐 구성.py
@@ -0,0 +1,23 @@
+# 정수 N, M을 입력받기
+n, m = map(int, input().split())
+# N개의 화폐 단위 정보를 입력받기
+array = []
+for i in range(n):
+    array.append(int(input()))
+
+# 한 번 계산된 결과를 저장하기 위한 DP 테이블 초기화
+d = [10001] * (m + 1)
+
+# Dynamic Programming 진행(보텀업)
+d[0] = 0
+for i in range(n):
+    for j in range(array[i], m + 1):
+        if d[j - array[i]] != 10001:  # (i - k)원을 만드는 방법이 존재하는 경우
+            d[j] = min(d[j], d[j - array[i]] + 1) # 사실 이 부분이 계속해서 호출되면 바로 위 if문은 필요가 없다.
+
+# 계산된 결과 출력
+if d[m] == 10001: # 최종적으로 M원을 만드는 방법이 없는 경우
+    print(-1)
+else:
+    print(d[m])
+
