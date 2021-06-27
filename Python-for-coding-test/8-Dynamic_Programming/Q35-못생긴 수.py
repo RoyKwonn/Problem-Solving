a2dc09531df8e7e712a03f7ed1ddc3e8f279acda
Seokhwan-Kwon <sychar05@gmail.com>
2021-06-27 22:52:55 +0900

    2021-06-27

---
+++ Python-for-coding-test/8-Dynamic_Programming/Q35-못생긴 수.py
@@ -0,0 +1,27 @@
+
+# 오직 2, 3, 5 만을 소인수로 가지는 수
+
+n = int(input())
+
+cnt = 0
+i = 0
+
+dp = [0] * n
+
+i2 = i3 = i5 = 0
+next2, next3, next5 = 2, 3, 5
+
+for i in range(1, n):
+    dp[i] = min(next2, next3, next5)
+
+    if dp[i] == next2:
+        i2 += 1
+        next2 = dp[i2] * 2
+    if dp[i] == next3:
+        i3 += 1
+        next3 = dp[i3] * 3
+    if dp[i] == next5:
+        i5 += 1
+        next5 = dp[i5] * 5
+
+print(dp[n-1])
