7021d96a16e5e49051ff0eec446f58e6e3422426
Seokhwan-Kwon <sychar05@gmail.com>
2021-07-25 15:41:19 +0900

    2021-07-25

---
+++ Python-for-coding-test/3-Greedy/Q01-모험가 길드.cpp
@@ -0,0 +1,21 @@
+//
+//  Q01-모험가 길드.cpp
+//  Problem_solving
+//
+//  Created by Seokhwan Kwon on 2021/07/25.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    vector<int> v;
+    int cnt = 0;
+    int result = 0;
+    
+    cin >> n;
+    
+    
+}
