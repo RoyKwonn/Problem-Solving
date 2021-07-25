58de4fdad5f5e1a19f3a978231136ccd1cc8ecb0
Seokhwan-Kwon <sychar05@gmail.com>
2021-07-25 19:04:13 +0900

    2021-07-25

---
+++ Python-for-coding-test/3-Greedy/Q03-문자열 뒤집기.cpp
@@ -0,0 +1,23 @@
+//  Created by Seokhwan Kwon on 2021/07/25.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    string s;
+    cin >> s;
+    int cnt = 0;
+    for(int i = 0; i < s.size()-1; i++) {
+        if(s[i] != s[i+1]) {
+            cnt++;
+        }
+    }
+    
+    printf("%d\n",  (cnt+1) / 2);
+    
+    return 0;
+}
+
